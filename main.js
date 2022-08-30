ce9e2705829d1cc005c3b36bbe3afe27d8180b19
Roshan_Kumar <roshan@gmail.com>
2022-08-30 13:19:09 +0530

    Initial Stage of the project is completed

---
+++ main.js
@@ -0,0 +1,14 @@
+function getPassword(){{
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?><:{}[]";
+    var passwordLength = "16";
+    var password = ""; 
+
+    for(var i = 0; i<passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber+1); 
+    }
+    document.getElementById("password").value = password
+}}
+let password = document.getElementById("btn")
+
+password.addEventListener('click',getPassword);
